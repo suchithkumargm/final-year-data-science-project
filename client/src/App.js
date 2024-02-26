ebdf88b05843f1def54d34a756cc38c8d2e5d118
Suchith Kumar G M <suchithkumargm@gmail.com>
2024-02-26 06:55:06 +0000

    initialised react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './components/Header/Header.js';
+
+const App = () => {
+
+	return (
+		<div className='main'>
+			<Header />
+		</div>
+	);
+}
+
+export default App;
