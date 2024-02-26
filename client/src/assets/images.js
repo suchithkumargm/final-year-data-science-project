1cb0721e20050dd0b51b9cd6ab42c7e645454e31
Suchith Kumar G M <suchithkumargm@gmail.com>
2024-02-26 13:43:37 +0530

    add images folder and file

---
+++ client/src/assets/images.js
@@ -0,0 +1,17 @@
+import burger from './images/burger.jpeg';
+import butterchicken from './images/butterchicken.jpeg';
+import dosa from './images/dosa.jpeg';
+import fries from './images/fries.jpeg';
+import gobi from './images/gobi.jpeg';
+import golgappa from './images/golgappa.jpeg';
+import gulabjamun from './images/gulabjamun.jpeg';
+import idli from './images/idli.jpeg';
+import jalebi from './images/jalebi.jpeg';
+import masalapoori from './images/masalapoori.jpeg';
+import naan from './images/naan.jpeg';
+import pizza from './images/pizza.avif';
+import samosa from './images/samosa.jpeg';
+
+export default {
+    burger, butterchicken, dosa, fries, gobi, golgappa, gulabjamun, idli, jalebi, masalapoori, naan, pizza, samosa
+}
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Home.css';
+import images from '../../assets/images.js';
 
 const Home = () => {
     return (
